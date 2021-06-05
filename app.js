0a527aa9d872c1272ce51e1bed11254307e5e0de
eselse <ilikewebdevelopment@gmail.com>
2021-06-05 22:01:50 +0400

    add functions for create user and format the money

--- app.js
+++ app.js
@@ -21,16 +21,44 @@ async function getRandomUser() {
     money: Math.floor(Math.random() * 1_000_000),
   };
 
-  console.log(newUser);
-
   addData(newUser);
 }
 
 // Add new obj to data arr
 function addData(obj) {
   data.push(obj);
+
+  updateDOM();
 }
 
-getRandomUser();
-getRandomUser();
-getRandomUser();
+function updateDOM(providedData = data) {
+  // Clear main div
+  main.innerHTML = "<h2><strong>Person</strong> Wealth</h2>";
+
+  providedData.forEach((item) => {
+    const element = document.createElement("div");
+    element.classList.add("person");
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(
+      item.money
+    )}`;
+    main.appendChild(element);
+  });
+}
+
+// Format number as money - https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
+function formatMoney(num) {
+  var formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+
+    // These options are needed to round to whole numbers if that's what you want.
+    //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+    //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+  });
+
+  return formatter.format(num); /* $2,500.00 */
+}
+
+// Event Listeners
+addUserBtn.addEventListener("click", getRandomUser);
